d3c9ca52eaf078d404ae925be2a8a8726095da2f
Alyona Yermakova <alyona.yermakova@gmail.com>
2014-07-02 22:17:08 +0600

    Add entity

--- src/main/java/entity/Paragraph.java
+++ src/main/java/entity/Paragraph.java
@@ -1,4 +1,19 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Paragraph {
+    List<Sentence> sentences = new ArrayList<Sentence>();
+
+    public Paragraph(List<Sentence> sentences) {
+        this.sentences = sentences;
+    }
+
+    @Override
+    public String toString() {
+        return "Paragraph{" +
+                "sentences:" + sentences +
+                '}';
+    }
 }
--- src/main/java/entity/Text.java
+++ src/main/java/entity/Text.java
@@ -1,5 +1,21 @@
 package entity;
 
-public class Text {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Text {
+    List<Paragraph> paragraphList = new ArrayList<Paragraph>();
+
+    public Text(List<Paragraph> paragraphList) {
+        this.paragraphList = paragraphList;
+    }
+
+    public Text (String text){}
+
+    @Override
+    public String toString() {
+        return "Text{" +
+                "paragraps:" + paragraphList +
+                '}';
+    }
 }
